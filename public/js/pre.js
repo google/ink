0ddafae62d8b46ec8149f133ac5ce9d7752ceaf4
pikacuh <48255487+pikacuh@users.noreply.github.com>
2019-04-08 19:16:35 +0800

    Update pre.js

--- public/js/pre.js
+++ public/js/pre.js
@@ -1,3 +1,40 @@
+{
+  "project_info": {
+    "project_number": "698118686594",
+    "firebase_url": "https://iklan-b84cd.firebaseio.com",
+    "project_id": "iklan-b84cd",
+    "storage_bucket": "iklan-b84cd.appspot.com"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:698118686594:android:48e9583505e6a749",
+        "android_client_info": {
+          "package_name": "pika.android"
+        }
+      },
+      "oauth_client": [],
+      "api_key": [
+        {
+          "current_key": "AIzaSyDXsnXceC5mz9ycEasAHIK3q6ka63_mkg4"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": [
+            {
+              "client_id": "698118686594-t7t1ktsjadhsdre9b7k8ijolrkhr65nd.apps.googleusercontent.com",
+              "client_type": 3
+            }
+          ]
+        }
+      },
+      "admob_app_id": "ca-app-pub-3844337192222340~8166374561"
+    }
+  ],
+  "configuration_version": "1"
+}
+//
 // Copyright 2018 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
