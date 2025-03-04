ced18700d757f587f34b60d25ac48e2ef64c5e66
Ink Open Source <ink-open-source@google.com>
2025-03-03 21:09:02 -0800

    Automated Code Change
    PiperOrigin-RevId: 733168693

--- ink/geometry/mesh.cc
+++ ink/geometry/mesh.cc
@@ -25,6 +25,7 @@
 #include "absl/algorithm/container.h"
 #include "absl/log/absl_check.h"
 #include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/substitute.h"
 #include "absl/types/span.h"
 #include "ink/geometry/internal/mesh_packing.h"
