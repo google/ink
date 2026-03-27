e09027697efbccc62033ab9e2bf77be66eefdffb
Samuel Freilich <sfreilich@google.com>
2026-03-27 15:32:57 -0700

    Don't use alwayslink in KMP interface targets
    Fix a missing header.
    
    PiperOrigin-RevId: 890666122

--- ink/geometry/internal/jni/angle_native.cc
+++ ink/geometry/internal/jni/angle_native.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "ink/geometry/internal/jni/angle_native.h"
+
 #include "ink/geometry/angle.h"
 
 // C-compatible library implementation for Kotlin-native bindings.
