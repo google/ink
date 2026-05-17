aeb07ae60576666035ff076deff76f3e134139d6
Ink Open Source <ink-open-source@google.com>
2026-05-16 22:22:26 -0700

    Automated Code Change
    PiperOrigin-RevId: 916653824

--- ink/geometry/internal/algorithms_test.cc
+++ ink/geometry/internal/algorithms_test.cc
@@ -20,7 +20,6 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "absl/container/inlined_vector.h"
-#include "absl/status/status.h"
 #include "absl/status/status_matchers.h"
 #include "absl/status/statusor.h"
 #include "absl/types/span.h"
