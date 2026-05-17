dca86ec0eef2dce69e06ed6d31b60d85deacf939
Ink Open Source <ink-open-source@google.com>
2026-05-16 23:01:30 -0700

    Automated Code Change
    PiperOrigin-RevId: 916663415

--- ink/geometry/mesh_test_helpers_test.cc
+++ ink/geometry/mesh_test_helpers_test.cc
@@ -18,7 +18,6 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "absl/status/status.h"
 #include "absl/status/status_matchers.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
